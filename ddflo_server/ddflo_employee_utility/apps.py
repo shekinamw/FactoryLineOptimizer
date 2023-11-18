aea2e9db7da4cb550b0eccfc8af5d5ad969c15d1
DemiwizAces <erdmriosnadeau@gmail.com>
2023-11-18 18:20:27 -0500

    Divided project into applications with specific roles

---
+++ ddflo_server/ddflo_employee_utility/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DdfloEmployeeUtilityConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ddflo_employee_utility'
---
+++ ddflo_server/ddflo_simulated_sensor_data_factory/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DdfloSimulatedSensorDataFactoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ddflo_simulated_sensor_data_factory'
